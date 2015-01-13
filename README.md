f288919fc8f4dac751b3828695a22303ebc4c073
TheBronx <salmarina5@gmail.com>
2015-01-13 21:39:29 +0100

    plugin init
    plugin init

---
+++ README.md
@@ -0,0 +1,8 @@
+# Reputation Rules Plugin for NodeBB
+
+## User Rules
+ - cannot vote more than 5 times per day
+ - cannot vote more than 1 post of the same user
+ - TODO...
+
+## Other rules
---
+++ library.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var plugin = {};
+
+plugin.upvote = function(pid, uid, callback) {
+	//TODO
+};
+
+plugin.downvote = function(pid, uid, callback) {
+	//TODO
+};
+
+module.exports = plugin;
