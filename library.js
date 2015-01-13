4855b1173a8ba5a53dd2c4b3139945bfb5f50441
TheBronx <salmarina5@gmail.com>
2015-01-13 23:53:54 +0100

    testing hook params and data recovery
    testing hook params and data recovery

--- library.js
+++ library.js
@@ -1,13 +1,51 @@
 "use strict";
 
-var plugin = {};
+var plugin = {},
+	User = module.parent.require('./user'),
+	Posts = module.parent.require('./posts');
 
-plugin.upvote = function(pid, uid, callback) {
-	//TODO
+plugin.upvote = function(vote) {
+	console.log('user id: ' + vote.uid + ', post id: ' + vote.pid + ', current: ' + vote.current);
+
+	User.getUserData(vote.uid, function(err, user) {
+		if (err) {
+			console.log(err);
+			return;
+		}
+
+		console.log(user);
+	});
+
+	Posts.getPostData(vote.pid, function(err, post) {
+		if (err) {
+			console.log(err);
+			return;
+		}
+
+		console.log(post);
+	});
 };
 
-plugin.downvote = function(pid, uid, callback) {
-	//TODO
+plugin.downvote = function(vote) {
+	console.log('user id: ' + vote.uid + ', post id: ' + vote.pid + ', current: ' + vote.current);
+
+	User.getUserData(vote.uid, function(err, user) {
+		if (err) {
+			console.log(err);
+			return;
+		}
+
+		console.log(user);
+	});
+
+	Posts.getPostData(vote.pid, function(err, post) {
+		if (err) {
+			console.log(err);
+			return;
+		}
+
+		console.log(post);
+	});
 };
 
 module.exports = plugin;
