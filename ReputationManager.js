54db29da1b2af273b8d758810711c1e1938eb1a9
TheBronx <salmarina5@gmail.com>
2015-01-17 17:34:41 +0100

    rules 1 and 2 ready
    rules 1 and 2 ready

--- ReputationManager.js
+++ ReputationManager.js
@@ -1,16 +1,78 @@
 'use strict';
 
+/*
+ #1 para votar positivo necesitas tener 20 mensajes y llevar 7 días registrado (TODO: logros)
+ #2 para votar negativo necesitas tener 50 mensajes,  llevar 15 días registrado y tener 10 de reputación
+ #3 cada voto negativo te resta 1 de reputación a ti también
+ #4 un usuario no puede votar más de x veces al día. sea x = reputación/10. con unos mínimo de 5 y un máximo de 50
+ #5 la reputación no funciona en algunas secciones del foro
+ #6 no puedes votar a la misma persona más de 1 vez al mes
+ #7 no puedes votar más de 5 mensajes de un mismo hilo
+ #8 el valor de un voto positivo depende de la reputación del votante: valor = 1 + floor(reputación/10)
+ */
+
+var MIN_POSTS_TO_UPVOTE = 20;
+var MIN_DAYS_TO_UPVOTE = 7;
+
+var MIN_POSTS_TO_DOWNVOTE = 50;
+var MIN_DAYS_TO_DOWNVOTE = 15;
+var MIN_REPUTATION_TO_DOWNVOTE = 10;
+
 var ReputationManager = function() {
 	var _this = this;
 	var rules = {};
 
 	this.userCanUpvotePost = function(user, post) {
+		if (!hasEnoughPostsToUpvote(user.postcount)) {
+			return false;
+		}
+
+		if (!isOldEnoughToUpvote(user.joindate)) {
+			return false;
+		}
+
 		return true;
 	};
 
 	this.userCanDownvotePost = function(user, post) {
+		if (!hasEnoughPostsToDownvote(user.postcount)) {
+			return false;
+		}
+
+		if (!isOldEnoughToDownvote(user.joindate)) {
+			return false;
+		}
+
+		if (!hasEnoughReputationToDownvote(user.reputation)) {
+			return false;
+		}
+
 		return true;
 	};
 };
+
+function hasEnoughPostsToUpvote(postcount) {
+	return postcount > MIN_POSTS_TO_UPVOTE;
+}
+
+function isOldEnoughToUpvote(registrationTimestamp) {
+	var now = new Date();
+	var xDaysAgo = now.getTime() - MIN_DAYS_TO_UPVOTE * 24 * 60 * 60 * 1000;
+	return registrationTimestamp < xDaysAgo;
+}
+
+function hasEnoughPostsToDownvote(postcount) {
+	return postcount > MIN_POSTS_TO_DOWNVOTE;
+}
+
+function isOldEnoughToDownvote(registrationTimestamp) {
+	var now = new Date();
+	var xDaysAgo = now.getTime() - MIN_DAYS_TO_DOWNVOTE * 24 * 60 * 60 * 1000;
+	return registrationTimestamp < xDaysAgo;
+}
+
+function hasEnoughReputationToDownvote(reputation) {
+	return reputation > MIN_REPUTATION_TO_DOWNVOTE;
+}
 
 module.exports = ReputationManager;
--- library.js
+++ library.js
@@ -16,6 +16,12 @@ plugin.upvote = function(vote) {
 		}
 
 		console.log('user can upvote post? ' + ReputationManager.userCanUpvotePost(data.user, data.post));
+
+		if (!ReputationManager.userCanUpvotePost(data.user, data.post)) {
+			//TODO remove vote, without triggering the unvote hook
+		} else {
+			//TODO calculate vote value and increase rep for post author
+		}
 	});
 };
 
@@ -30,6 +36,12 @@ plugin.downvote = function(vote) {
 		}
 
 		console.log('user can downvote post? ' + ReputationManager.userCanDownvotePost(data.user, data.post));
+
+		if (!ReputationManager.userCanDownvotePost(data.user, data.post)) {
+			//TODO remove downvote
+		} else {
+			//TODO decrease user's reputation by 1 (if you downvote, you loose rep)
+		}
 	});
 };
 
