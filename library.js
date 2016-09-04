383588049181ea427de230d14f9ae9f16a13f660
TheBronx <salmarina5@gmail.com>
2016-09-04 12:06:45 +0200

    fix comma in package.json
    and update library version to 1.0.4

--- library.js
+++ library.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var plugin = {'settingsVersion': '1.0.3'},
+var plugin = {'settingsVersion': '1.0.4'},
     db = module.parent.require('./database'),
     users = module.parent.require('./user'),
     meta = module.parent.require('./meta'),
