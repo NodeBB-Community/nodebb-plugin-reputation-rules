f2a7ba417360d15e6012eb506f8fd0dac8ba6075
Salva <slabajos@idealista.com>
2020-05-31 23:42:35 +0200

    new config and admin template #5

--- Config.js
+++ Config.js
@@ -5,6 +5,7 @@ var MIN_POSTS_TO_UPVOTE = 20,
     MIN_REPUTATION_TO_DOWNVOTE = 10,
     MAX_VOTES_PER_USER_AND_THREAD = 5,
     MAX_VOTES_TO_SAME_USER_PER_MONTH = 1,
+    MAX_DOWNVOTES_PER_DAY = 5, // 0 means disabled
     UPVOTE_EXTRA_PERCENTAGE = 5,
     DOWNVOTE_EXTRA_PERCENTAGE = 5,
     DOWNVOTE_PENALIZATION = 1,
@@ -46,6 +47,9 @@ var Config = {
             calculatedVotesPerUser = MAX;
         }
         return calculatedVotesPerUser;
+    },
+    maxDownvotesPerDay: function () {
+        return MAX_DOWNVOTES_PER_DAY;
     },
     upvoteExtraPercentage: function () {
         return UPVOTE_EXTRA_PERCENTAGE;
@@ -97,6 +101,7 @@ var Config = {
         settings.minReputationToDownvote = MIN_REPUTATION_TO_DOWNVOTE;
         settings.maxVotesPerUserInThread = MAX_VOTES_PER_USER_AND_THREAD;
         settings.maxVotesToSameUserInMonth = MAX_VOTES_TO_SAME_USER_PER_MONTH;
+        settings.maxDownvotesPerDay = MAX_DOWNVOTES_PER_DAY;
         settings.upvoteExtraPercentage = UPVOTE_EXTRA_PERCENTAGE;
         settings.downvoteExtraPercentage = DOWNVOTE_EXTRA_PERCENTAGE;
         settings.downvotePenalization = DOWNVOTE_PENALIZATION;
@@ -115,6 +120,7 @@ var Config = {
         MIN_REPUTATION_TO_DOWNVOTE = settings.minReputationToDownvote;
         MAX_VOTES_PER_USER_AND_THREAD = settings.maxVotesPerUserInThread;
         MAX_VOTES_TO_SAME_USER_PER_MONTH = settings.maxVotesToSameUserInMonth;
+        MAX_DOWNVOTES_PER_DAY = settings.maxDownvotesPerDay;
         UPVOTE_EXTRA_PERCENTAGE = settings.upvoteExtraPercentage;
         DOWNVOTE_EXTRA_PERCENTAGE = settings.downvoteExtraPercentage;
         DOWNVOTE_PENALIZATION = settings.downvotePenalization;
@@ -133,4 +139,3 @@ function intArray(arr) {
 }
 
 module.exports = Config;
-
